55f345eaf29e6ce41f31ebb06340b45a8b9bbcf8
hendrik <hendrik.debruin@iit.it>
2019-10-31 08:55:55 +0100

    initial definition of periodic app layer component

---
+++ include/application_framework/components/periodic_app_layer_component.hpp
@@ -0,0 +1,24 @@
+#ifndef PERIODIC_APP_LAYER_COMPONENT_HPP_RY9LWBZG
+#define PERIODIC_APP_LAYER_COMPONENT_HPP_RY9LWBZG
+
+#include "application_framework/components/app_layer_component.hpp"
+
+#include <chrono>
+#include <atomic>
+
+class PeriodicAppLayerComponent : public AppLayerComponent
+{
+public:
+	typedef std::chrono::duration<double, std::ratio<1, 1'000'000>> period_t;
+public:
+	PeriodicAppLayerComponent(const period_t&);
+
+	virtual Status run() override;
+	virtual void run(const std::chrono::system_clock::time_point&) = 0;
+
+private:
+	const period_t period;
+	std::atomic_bool should_run;
+};
+
+#endif /* end of include guard: PERIODIC_APP_LAYER_COMPONENT_HPP_RY9LWBZG */
---
+++ src/application_framework/components/periodic_app_layer_component.cpp
@@ -0,0 +1,39 @@
+#include <thread>
+#include "application_framework/components/periodic_app_layer_component.hpp"
+
+// =============================================================================
+// Constructors
+// =============================================================================
+PeriodicAppLayerComponent::PeriodicAppLayerComponent(const period_t &inPeriod) :
+	period(inPeriod),
+	should_run(false)
+{ }
+
+// =============================================================================
+// Interface Override Functions
+// =============================================================================
+AppLayerComponent::Status PeriodicAppLayerComponent::run()
+{
+	setStatus(Status::RUNNING);
+	this->should_run = true;
+	do
+	{
+		// Calculate when the next period needs to start
+		auto next_loop_time = this->period + std::chrono::system_clock::now();
+
+		// Run one epoch
+		run(std::chrono::system_clock::now());
+
+		// Check realtime
+		if(std::chrono::system_clock::now() > next_loop_time)
+		{
+			setStatus(Status::BREAKING_REALTIME);
+		}
+
+		TODO("use realtime sleep here")
+		std::this_thread::sleep_until(next_loop_time);
+
+	}while(this->should_run);
+
+	return this->getStatus();
+}
