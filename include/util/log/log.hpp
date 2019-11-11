26aa4a522f1581e0b8d2673785d3ecb2ca05c8ad
hendrik <hendrik.debruin@iit.it>
2019-11-11 11:11:49 +0100

    put logging implementation into private impl header

--- include/util/log/log.hpp
+++ include/util/log/log.hpp
@@ -2,85 +2,41 @@
 #define LOG_HPP_AMCC9JXS
 
 #include <sstream>
+#include <mutex>
 
+#include "util/log/impl/log.hpp"
+
+/// Logging namespace
+///
+/// This namespace contains util classes related to logging, as well as
+/// instances of those classes for use
 namespace logging
 {
-	// implementation namespace
-	namespace impl
-	{
-		enum class LogLevel
-		{
-			DEBUG,
-			INFO,
-			WARN,
-			ERROR,
-			FATAL
-		};
-
-		enum class StreamManipulator
-		{
-			ENDL
-		};
-
-		template <StreamManipulator manip>
-		class StreamManip{};
-
-		template <LogLevel loglevel>
-		class LogStream;
-
-		template <LogLevel loglevel>
-		class LogInput
-		{
-			friend class LogStream<loglevel>;
-		public:
-			LogInput<loglevel> &operator<< (const std::string& s)
-			{
-				this->ss << s;
-				return *this;
-			}
-
-			void operator<<(const StreamManip<StreamManipulator::ENDL>&)
-			{
-				LogStream<loglevel>::log(this->ss.str());
-			}
-
-		private:
-			LogInput() = default;
-			std::stringstream ss;
-		};
-
-		template <LogLevel loglevel>
-		class LogStream
-		{
-			friend class LogInput<loglevel>;
-		public:
-			LogInput<loglevel> operator<< (const std::string&s) const
-			{
-				logging::impl::LogInput<loglevel> input;
-				input.ss << s;
-				return input;
-			}
-		private:
-			static void log(const std::string &s)
-			{
-				std::cout << s << std::endl;
-			}
-		};
-
-	} // namespace impl
-
-
-	// static members for usage
+	/// Debug messages log stream
+	///
 	impl::LogStream<impl::LogLevel::DEBUG>  cdbg;
+
+	/// Stream for basic system logging, such as state changes
+	///
 	impl::LogStream<impl::LogLevel::INFO>   clog;
+
+	/// Stream for warnings that the user should be made aware of
+	///
 	impl::LogStream<impl::LogLevel::WARN>   cout;
+
+	/// Stream for non-fatal errors
+	///
 	impl::LogStream<impl::LogLevel::ERROR>  cerr;
+
+	/// Stream for fatal error messages
+	///
 	impl::LogStream<impl::LogLevel::FATAL>  cfatal;
 
+	/// Stream manipulator indicating the end of a log stream
+	///
 	impl::StreamManip<impl::StreamManipulator::ENDL> endl;
 
 } // namespace logging
 
-#include "util/log/log.tpp"
 
 #endif /* end of include guard: LOG_HPP_AMCC9JXS */
--- include/util/log/log.tpp
+++ include/util/log/log.tpp
@@ -1,39 +1,51 @@
-// #ifndef LOG_TPP_O9SQ6AB1
-// #define LOG_TPP_O9SQ6AB1
+#ifndef LOG_TPP_O9SQ6AB1
+#define LOG_TPP_O9SQ6AB1
 
-// #include "util/log/log.hpp"
+#include "util/log/log.hpp"
 
-// template <logging::impl::LogLevel loglevel>
-// logging::impl::LogInput<loglevel> operator<<
-// (
-// 	const logging::impl::LogStream<loglevel> &ls,
-// 	const std::string &s
-// )
-// {
-// 	logging::impl::LogInput<loglevel> input;
-// 	input.ss << s;
-// 	return input;
-// }
+#include <iostream>
+#include <iomanip>
+#include <ctime>
 
-// template <logging::impl::LogLevel loglevel>
-// logging::impl::LogInput<loglevel> &operator<<
-// (
-// 	const logging::impl::LogInput<loglevel> &li,
-// 	const std::string &s
-// )
-// {
-// 	li.ss << s;
-// 	return li;
-// }
+namespace logging
+{
+	namespace impl
+	{
+		template <LogLevel loglevel>
+		LogInput<loglevel> &LogInput<loglevel>::operator<<(const std::string &s)
+		{
+			this->ss << s;
+			return *this;
+		}
 
-// template <logging::impl::LogLevel loglevel>
-// void operator<<
-// (
-// 	const logging::impl::LogInput<loglevel> &li,
-// 	const logging::impl::StreamManip<logging::impl::StreamManipulator::ENDL>&
-// )
-// {
-// 	logging::impl::LogStream<loglevel>::log(li.ss.str());
-// }
-// #endif /* end of include guard: LOG_TPP_O9SQ6AB1 */
+		template <LogLevel loglevel>
+		void LogInput<loglevel>::operator<<
+		(
+			const StreamManip<StreamManipulator::ENDL>&
+		)
+		{
+			LogStream<loglevel>::log(this->ss.str());
+		}
 
+		template <LogLevel loglevel>
+		LogInput<loglevel> LogStream<loglevel>::operator<<(const std::string &s) const
+		{
+			logging::impl::LogInput<loglevel> input;
+
+			auto t = std::time(nullptr);
+			auto tm = *std::localtime(&t);
+			input.ss << std::put_time(&tm, "%d/%m/%Y %H:%M:%S");
+			input.ss << ": ";
+			input.ss << s;
+			return input;
+		}
+
+		template <LogLevel loglevel>
+		void LogStream<loglevel>::log(const std::string &s)
+		{
+			std::cout << s << std::endl;
+		}
+
+	}
+}
+#endif /* end of include guard: LOG_TPP_O9SQ6AB1 */
