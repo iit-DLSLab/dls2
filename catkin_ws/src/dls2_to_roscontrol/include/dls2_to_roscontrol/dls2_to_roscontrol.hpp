63848a5186575edb75389e541ed593a3af71b4af
gfink <geoff.fink@iit.it>
2019-11-14 18:41:47 +0100

    WIP: Add shell for dls to roscontrol block. Compile with catkin_make from the catkin_ws directory. Note compiles, but does not work

---
+++ catkin_ws/src/dls2_to_roscontrol/include/dls2_to_roscontrol/dls2_to_roscontrol.hpp
@@ -0,0 +1,28 @@
+#ifndef DLS2_TO_ROSCONTROL_H
+#define DLS2_TO_ROSCONTROL_H
+
+#include <ros/node_handle.h>
+#include <hardware_interface/robot_hw.h>
+#include <hardware_interface/joint_command_interface.h>
+#include <controller_interface/controller.h>
+#include <pluginlib/class_list_macros.h>
+
+namespace dls2_to_roscontrol {
+
+class Dls2ToRoscontrol : public controller_interface::Controller<hardware_interface::JointCommandInterface>
+{
+public:
+	Dls2ToRoscontrol();
+	bool init(hardware_interface::JointCommandInterface* hw, ros::NodeHandle& root_nh, ros::NodeHandle& controller_nh);
+	void starting(const ros::Time &time);
+	void stopping(const ros::Time &time);
+	void update(const ros::Time& time, const ros::Duration& period);
+private:
+	std::vector<hardware_interface::JointHandle> joint_commands_;
+};
+
+PLUGINLIB_DECLARE_CLASS(dls2_ros_control, Dls2ToRoscontrol, dls2_to_roscontrol::Dls2ToRoscontrol, controller_interface::ControllerBase);
+
+} //namespace dls2_to_roscontrol
+
+#endif // DLS2_TO_ROSCONTROL_H
