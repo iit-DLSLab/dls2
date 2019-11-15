b772e53947f7bc51fd0c58331167016394a7ac73
gfink <geoff.fink@iit.it>
2019-11-15 10:44:04 +0100

    Communication working with gazebo

--- catkin_ws/src/dls2_to_roscontrol/include/dls2_to_roscontrol/dls2_to_roscontrol.hpp
+++ catkin_ws/src/dls2_to_roscontrol/include/dls2_to_roscontrol/dls2_to_roscontrol.hpp
@@ -3,22 +3,22 @@
 
 #include <ros/node_handle.h>
 #include <hardware_interface/robot_hw.h>
-#include <hardware_interface/joint_command_interface.h>
+#include <dls_hardware_interface/joint_command_adv_interface.h>
 #include <controller_interface/controller.h>
 #include <pluginlib/class_list_macros.h>
 
 namespace dls2_to_roscontrol {
 
-class Dls2ToRoscontrol : public controller_interface::Controller<hardware_interface::JointCommandInterface>
+class Dls2ToRoscontrol : public controller_interface::Controller<hardware_interface::JointCommandAdvInterface>
 {
 public:
 	Dls2ToRoscontrol();
-	bool init(hardware_interface::JointCommandInterface* hw, ros::NodeHandle& root_nh, ros::NodeHandle& controller_nh);
+	bool init(hardware_interface::JointCommandAdvInterface* hw, ros::NodeHandle& root_nh, ros::NodeHandle& controller_nh);
 	void starting(const ros::Time &time);
 	void stopping(const ros::Time &time);
 	void update(const ros::Time& time, const ros::Duration& period);
 private:
-	std::vector<hardware_interface::JointHandle> joint_commands_;
+	std::vector<hardware_interface::JointCommandAdvHandle> joint_commands_;
 };
 
 PLUGINLIB_DECLARE_CLASS(dls2_ros_control, Dls2ToRoscontrol, dls2_to_roscontrol::Dls2ToRoscontrol, controller_interface::ControllerBase);
--- catkin_ws/src/dls2_to_roscontrol/src/dls2_to_roscontrol.cpp
+++ catkin_ws/src/dls2_to_roscontrol/src/dls2_to_roscontrol.cpp
@@ -7,25 +7,29 @@ Dls2ToRoscontrol::Dls2ToRoscontrol()
 	
 }
 
-bool Dls2ToRoscontrol::init(hardware_interface::JointCommandInterface *pJoint_command_interface, ros::NodeHandle &root_nh, ros::NodeHandle &controller_nh)
+bool Dls2ToRoscontrol::init(hardware_interface::JointCommandAdvInterface *pJoint_command_adv_interface, ros::NodeHandle &root_nh, ros::NodeHandle &controller_nh)
 {
-	if (!pJoint_command_interface)
+	if (!pJoint_command_adv_interface)
 	{
-		ROS_ERROR("JointCommandInterface is a null pointer");
+		ROS_ERROR("JointCommandAdvInterface is a null pointer");
 		return false;
 	}
 	int num_joints=12;
 	char joint_names[12][13] = {"lf_haa_joint","lf_hfe_joint","lf_kfe_joint","rf_haa_joint","rf_hfe_joint","rf_kfe_joint","lh_haa_joint","lh_hfe_joint","lh_kfe_joint","rh_haa_joint","rh_hfe_joint","rh_kfe_joint"};
 	for (int i = 0; i < num_joints; i++)
 	{
-		this->joint_commands_.push_back(pJoint_command_interface->getHandle(joint_names[i]));
+		joint_commands_.push_back(pJoint_command_adv_interface->getHandle(joint_names[i]));
 	}
 	return true;
 }
 
 void Dls2ToRoscontrol::update(const ros::Time &time, const ros::Duration &period)
 {
-	
+	for (auto jc : joint_commands_)
+	{
+		jc.setCommand(0,0,0);
+		jc.setCommandGains(1,1,1);
+	}
 }
 
 void Dls2ToRoscontrol::starting(const ros::Time &time) { }
