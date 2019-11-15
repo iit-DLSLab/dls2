d197e0347e783eac822c003930ffb213fbfc4f30
hendrik <hendrik.debruin@iit.it>
2019-11-15 10:49:16 +0100

    WIP topics and message types for low level estimation

---
+++ include/topics/low_level_estimation/body_acceleration.hpp
@@ -0,0 +1,31 @@
+/*******************************************************************************
+*                                                       ,----,                 *
+*                                                     .'   .' \                *
+*                                                   ,----,'    |               *
+*               ________  ___       ________        |    :  .  ;               *
+*              |\   ___ \|\  \     |\   ____\       ;    |.'  /                *
+*              \ \  \_|\ \ \  \    \ \  \___|_      `----'/  ;                 *
+*               \ \  \ \\ \ \  \    \ \_____  \       /  ;  /                  *
+*                \ \  \_\\ \ \  \____\|____|\  \     ;  /  /-,                 *
+*                 \ \_______\ \_______\____\_\  \   /  /  /.`|                 *
+*                  \|_______|\|_______|\_________\./__;      :                 *
+*                                     \|_________||   :    .'                  *
+*                                                 ;   | .'                     *
+*                                                 `---'                        *
+********************************************************************************
+* Author:            Hendrik de Bruin                                          *
+* Maintainer:        Hendrik de Bruin                                          *
+* author email:      hendrik.debruin@iit.it                                    *
+*******************************************************************************/
+#ifndef BODY_ACCELERATION_HPP_C7L56H8W
+#define BODY_ACCELERATION_HPP_C7L56H8W
+
+namespace topics
+{
+	namespace low_level_estimation
+	{
+		constexpr auto body_acceleration = "low_level_estimation/body_acceleration";
+	}
+}
+
+#endif /* end of include guard: BODY_ACCELERATION_HPP_C7L56H8W */
---
+++ include/topics/low_level_estimation/joint_state.hpp
@@ -0,0 +1,31 @@
+/*******************************************************************************
+*                                                       ,----,                 *
+*                                                     .'   .' \                *
+*                                                   ,----,'    |               *
+*               ________  ___       ________        |    :  .  ;               *
+*              |\   ___ \|\  \     |\   ____\       ;    |.'  /                *
+*              \ \  \_|\ \ \  \    \ \  \___|_      `----'/  ;                 *
+*               \ \  \ \\ \ \  \    \ \_____  \       /  ;  /                  *
+*                \ \  \_\\ \ \  \____\|____|\  \     ;  /  /-,                 *
+*                 \ \_______\ \_______\____\_\  \   /  /  /.`|                 *
+*                  \|_______|\|_______|\_________\./__;      :                 *
+*                                     \|_________||   :    .'                  *
+*                                                 ;   | .'                     *
+*                                                 `---'                        *
+********************************************************************************
+* Author:            Hendrik de Bruin                                          *
+* Maintainer:        Hendrik de Bruin                                          *
+* author email:      hendrik.debruin@iit.it                                    *
+*******************************************************************************/
+#ifndef JOINT_STATE_HPP_OCQAWZJG
+#define JOINT_STATE_HPP_OCQAWZJG
+
+namespace topics
+{
+	namespace low_level_estimation
+	{
+		constexpr auto joint_state = "low_level_estimation/joint_state";
+	}
+}
+
+#endif /* end of include guard: JOINT_STATE_HPP_OCQAWZJG */
