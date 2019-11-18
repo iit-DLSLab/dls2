9823b84b418027c408f35a46458abdbc27008e9c
gfink <geoff.fink@iit.it>
2019-11-18 11:43:26 +0100

    Add really simple prep gait generator

---
+++ src/gait_generator/prep_gait_generator.cpp
@@ -0,0 +1,65 @@
+/*******************************************************************************
+*                                                       ,----,                 *
+*                                                     .'   .' \                *
+*                                                   ,----,'    |               *
+*               ________  ___       ________        |    :  .  ;               *
+*              |\   ___ \|\  \     |\   ____\       ;    |.'  /                *
+*              \ \  \_|\ \ \  \    \ \  \___|_      `----'/  ;                 *
+*               \ \  \ \\ \ \  \    \ \_____  \       /  ;  /                  *
+*                \ \  \_\\ \ \  \____\|____|\  \     ;  /  /-,                 *
+*                 \ \_______\ \_______\____\_\  \   /  /  /.`|                 *
+*                  \|_______|\|_______|\_________\./__;      :                 *
+*                                     \|_________||   :    .'                  *
+*                                                 ;   | .'                     *
+*                                                 `---'                        *
+********************************************************************************
+* Author:            Hendrik de Bruin                                          *
+* Maintainer:        Hendrik de Bruin                                          *
+* author email:      hendrik.debruin@iit.it                                    *
+*******************************************************************************/
+#include "gait_generator/prep_gait_generator.hpp"
+#include "util/debug/debug.hpp"
+#include "geometry/pose.hpp"
+#include "util/log/log.hpp"
+
+PrepGaitGenerator::PrepGaitGenerator(const std::shared_ptr<Dog> &pDog) : GaitGenerator
+	(
+		pDog,
+		"prep_gait_generator",
+		std::chrono::duration<double>(1)
+	)
+{
+	logging::clog << "prep gait generator launched" << logging::endl;
+}
+
+PrepGaitGenerator::PrepGaitGenerator() : PrepGaitGenerator(std::make_shared<Dog>())
+{
+	logging::clog << "prep gait generator destroyed" << logging::endl;
+}
+
+void PrepGaitGenerator::run(const std::chrono::system_clock::time_point &time)
+{
+	logging::clog << "Prep Gait Generator Epoch" << logging::endl;
+	GaitSignal data;
+
+	Eigen::VectorXd q; q << -0.2, 0.7, -1.4, -0.2, 0.7, -1.4, -0.2, -0.7, 1.4, -0.2, -0.7, 1.4;
+	Eigen::VectorXd qd;	qd << 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0;
+	Eigen::VectorXd qdd; qdd << 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0;
+	data.desired_joint_state = q;
+	data.desired_joint_velocity = qd;
+	data.desired_joint_acceleration = qdd;
+
+	publishData(data);
+	time.time_since_epoch();
+}
+
+extern "C" GaitGenerator *create()
+{
+	auto p = new PrepGaitGenerator;
+	return p;
+}
+
+extern "C" void destroy(GaitGenerator *p)
+{
+	delete p;
+}
