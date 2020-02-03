bbef574f6aaa3bd347471818995f67b52e85350a
hendrik <hendrik.debruin@iit.it>
2020-02-03 10:23:55 +0100

    basic bash completion added

---
+++ scripts/dls-completion.bash
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+robot_specified=0
+layers_specified=0
+mode_specified=0
+version_specified=0
+help_specified=0
+core_specified=0
+
+robot_names=("hyq hyqreal")
+
+# TODO this is a stub function that can be made more complete
+_dls_completion()
+{
+	completions=()
+	# if [[ ${COMP_WORDS[COMP_CWORD]} =~ "--" ]]; then
+		if ((!robot_specified)); then
+			completions+=("--robot")
+		fi
+		if ((!layers_specified)); then
+			completions+=("--layers")
+		fi
+		if ((!mode_specified)); then
+			completions+=("--hardware")
+			completions+=("--simulation")
+		fi
+		if ((!core_specified)); then
+			completions+=("--core")
+		fi
+		if ((!help_specified)); then
+			completions+=("--help")
+		fi
+		if ((!version_specified)); then
+			completions+=("--version")
+		fi
+	# fi
+	COMPREPLY=($(compgen -W "${completions[*]}" -- "${COMP_WORDS[COMP_CWORD]}"))
+}
+
+complete -F _dls_completion dls
