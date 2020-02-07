b65957b08e89ccb3c4bbf717f7232e0aefd5bbe1
hendrik <hendrik.debruin@iit.it>
2020-02-07 09:35:32 +0100

    Further cleanup of src/CMakeLists.txt

--- cmake/dls_deploy.cmake
+++ cmake/dls_deploy.cmake
@@ -10,3 +10,29 @@ set(CPACK_PACKAGE_VERSION_MAJOR ${MAJOR})
 set(CPACK_PACKAGE_VERSION_MINOR ${MINOR})
 set(CPACK_PACKAGE_VERSION_PATCH ${PATCH})
 include(CPack)
+
+# ==============================================================================
+# Target Installation
+# ==============================================================================
+# Target installation macro
+macro(dls_install TARGET)
+	install(TARGETS ${TARGET}
+		LIBRARY DESTINATION lib/dls2
+		ARCHIVE DESTINATION lib
+		RUNTIME DESTINATION ${DLS_INSTALL_RUNTIME_DIR}
+		PUBLIC_HEADER DESTINATION dls/include
+	)
+endmacro()
+
+# Header file installation
+install(
+	DIRECTORY
+		${CMAKE_SOURCE_DIR}/include/dls2/
+		${CMAKE_SOURCE_DIR}/gen/include/dls2/
+	DESTINATION
+		include/dls2
+	FILES_MATCHING
+		PATTERN "*.h"
+		PATTERN "*.hpp"
+		PATTERN "*.tpp"
+)
