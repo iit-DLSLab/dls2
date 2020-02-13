1038414e57c28705c517b9ed8aa1db3c5cf470af
hendrik <hendrik.debruin@iit.it>
2020-02-13 10:52:00 +0100

    more docs

---
+++ cmake/dls_docs.cmake
@@ -0,0 +1,7 @@
+function(dls_markdown_to_man MARKDOWN_FILE MAN_FILE)
+	execute_process(
+		COMMAND
+			pandoc ${MARKDOWN_FILE} -t man -o ${MARKDOWN_FILE}.man
+	)
+	set(${MAN_FILE} ${MARKDOWN_FILE}.man PARENT_SCOPE)
+endfunction()
