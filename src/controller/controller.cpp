f9a926d8bab0bbeb6536950f1ece6cf63152a63b
hendrik <hendrik.debruin@iit.it>
2020-03-09 13:43:47 +0100

    fix LD path

--- src/controller/controller.cpp
+++ src/controller/controller.cpp
@@ -58,7 +58,10 @@ Controller::Controller
 	blind_state_listener(std::shared_ptr<Controller>(this,[](Controller*){})),
 	control_signal_topic(std::string(topics::control_signal_base) + name_),
 	publisher(control_signal_topic)
-{ }
+{
+	std::cout << "controller is publishing on topic: '"
+		<< topics::control_signal_base + name_ << "'" << std::endl;
+}
 
 // =============================================================================
 // Implementation
