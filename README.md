ebe4782387a9365a00eecdb73c68ec82e2651284
hendrik <hendrik.debruin@iit.it>
2020-05-11 14:30:38 +0200

    automatically run setup.sh from cmake

--- README.md
+++ README.md
@@ -26,10 +26,6 @@ Building
 Development
 --------------------------------------------------------------------------------
 
-First, setup the git hooks:
-
-	$ ./setup.sh
-
 Build the debug project using:
 
 	$ mkdir Debug
--- setup.sh
+++ setup.sh
@@ -1,8 +1,23 @@
 #!/usr/bin/env bash
 
 # ==============================================================================
+# Test for lock file
+# ==============================================================================
+# If the lock file exists, this script has been run in the current copy of the
+# repository. Do not run again
+project_root=$(git rev-parse --show-toplevel)
+if [ -e $project_root/.setup.lock ]; then
+	exit 0
+fi
+
+# ==============================================================================
 # Githooks
 # ==============================================================================
 echo -- Installing githooks
 rm -r .git/hooks
 ln -sf ../githooks .git/hooks
+
+# ==============================================================================
+# Create Lock File
+# ==============================================================================
+echo "This file tracks whether the setup.sh script has been run" > $project_root/.setup.lock
