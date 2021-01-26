63a4bdd286f10a6b21e5ae4fcfaa742141733881
Hendrik de Bruin <hendrik.debruin@iit.it>
2021-01-26 10:44:58 +0100

    WIP: estimator module

--- modules/estimator/src/estimator.cpp
+++ modules/estimator/src/estimator.cpp
@@ -21,34 +21,4 @@ using namespace dls;
 // =============================================================================
 Estimator::Estimator(const ID_t &inID, const period_t &period) :
 	PeriodicAppLayerComponent(inID, period)
-	// ID(inID)
 { }
-
-// =============================================================================
-// Interface Override Functions
-// =============================================================================
-// AppLayerComponent::Status Estimator::run()
-// {
-// 	TODO("Unimplemented")
-// 	return getStatus();
-// }
-
-// AppLayerComponent::Status Estimator::eStop()
-// {
-// 	TODO("Unimplemented")
-// 	return getStatus();
-// }
-
-// AppLayerComponent::Status Estimator::stop()
-// {
-// 	TODO("Unimplemented")
-// 	return getStatus();
-// }
-
-// =============================================================================
-// Implementation
-// =============================================================================
-// Estimator::ID_t Estimator::getID() const
-// {
-// 	return this->ID;
-// }
