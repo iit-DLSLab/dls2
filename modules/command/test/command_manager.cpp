2339d68d49a134e5734dbf741fecc5054df3b1e7
Hendrik de Bruin <hendrik.debruin@iit.it>
2021-02-02 13:50:27 +0100

    test: pulled some tests from the dds test branch

---
+++ modules/command/test/command_manager.cpp
@@ -0,0 +1,37 @@
+#include "dls2/command/command.hpp"
+#include <catch2/catch.hpp>
+
+using namespace dls;
+
+// These tests aren't testing for any specific functionality
+// sometimes, when changing the version of fastdds, some things will break
+// randomly. These tests just build various objects in the code base that may
+// use dds functionality. If these tests segfault, it can give a quick
+// indication of where things are going wrong in new versions
+//
+// These tests are therefore very coarse grained, aimed at only giving an
+// initial direction for investigation.
+TEST_CASE("Creating a command manager works", "[command manager]")
+{
+	dls::impl::initFastdds();
+	CommandManager cm;
+}
+
+TEST_CASE("Creating a command manager and adding a command works", "[command manager]")
+{
+	CommandManager cm;
+
+	cm.addCommand<void, ARGVOID>
+	(
+		"test",
+		"dummy_command",
+		"no docs required",
+		std::function<void(ARGVOID)>
+		{
+			[&](ARGVOID)
+			{
+				// empty command
+			}
+		}
+	);
+}
