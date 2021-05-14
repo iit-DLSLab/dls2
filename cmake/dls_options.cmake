8f874bdf3f197cb216e9b96403e62d9144895d4d
gfink <geoff.fink@iit.it>
2021-05-14 09:58:43 +0000

    Make Release target default

--- cmake/dls_options.cmake
+++ cmake/dls_options.cmake
@@ -1,3 +1,7 @@
+if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
+	set(CMAKE_BUILD_TYPE "Release" CACHE STRING "Choose the type of build." FORCE)
+	set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "Debug" "Release")
+endif()                                                       
 # ==============================================================================
 # PATHS
 # ==============================================================================
