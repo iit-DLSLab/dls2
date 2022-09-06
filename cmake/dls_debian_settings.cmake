bf382518d1138a02f3c67fe901ca990da6167f0e
Douglas Bertol <douglas.bertol@iit.it>
2022-09-06 13:05:56 +0200

    fix: corrected dls2 package dependency robotlib to dls-robotlib

--- cmake/dls_debian_settings.cmake
+++ cmake/dls_debian_settings.cmake
@@ -13,7 +13,7 @@ set(CPACK_DEB_COMPONENT_INSTALL                 ON)
 
 set(CPACK_DEBIAN_DLS_DEV_PACKAGE_NAME           "dls2-dev" CACHE INTERNAL "")
 set(CPACK_DEBIAN_DLS_DEV_FILE_NAME              "dls2-dev-${PROJECT_VERSION}.deb" CACHE INTERNAL "")
-set(CPACK_DEBIAN_DLS_DEV_PACKAGE_DEPENDS        "dls2-runtime, dls-messages-dev, robotlib-dev, dls-param-server-dev" CACHE INTERNAL "")
+set(CPACK_DEBIAN_DLS_DEV_PACKAGE_DEPENDS        "dls2-runtime, dls-messages-dev, dls-robotlib-dev, dls-param-server-dev" CACHE INTERNAL "")
 
 set(CPACK_DEBIAN_DLS_DOC_PACKAGE_NAME          "dls2-doc" CACHE INTERNAL "")
 set(CPACK_DEBIAN_DLS_DOC_FILE_NAME             "dls2-doc-${PROJECT_VERSION}.deb" CACHE INTERNAL "") 
@@ -21,7 +21,7 @@ set(CPACK_DEBIAN_DLS_DOC_PACKAGE_DEPENDS       "" CACHE INTERNAL "")
 
 set(CPACK_DEBIAN_DLS_RUNTIME_PACKAGE_NAME       "dls2-runtime" CACHE INTERNAL "")
 set(CPACK_DEBIAN_DLS_RUNTIME_FILE_NAME          "dls2-runtime-${PROJECT_VERSION}.deb" CACHE INTERNAL "")
-set(CPACK_DEBIAN_DLS_RUNTIME_PACKAGE_DEPENDS    "dls-messages, robotlib, dls-param-server" CACHE INTERNAL "")
+set(CPACK_DEBIAN_DLS_RUNTIME_PACKAGE_DEPENDS    "dls-messages, dls-robotlib, dls-param-server" CACHE INTERNAL "")
 
 get_cmake_property(CPACK_COMPONENTS_ALL COMPONENTS)
 list(REMOVE_ITEM CPACK_COMPONENTS_ALL "Unspecified")
