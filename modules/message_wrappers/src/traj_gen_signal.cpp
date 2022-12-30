699c4a1add5e4247c3adc34cb1c0edf88fd96850
Marco Marchitto <marco.marchitto@iit.it>
2022-12-30 19:08:26 +0100

    adding normal_force_max/min + name refactoring

--- modules/message_wrappers/src/traj_gen_signal.cpp
+++ modules/message_wrappers/src/traj_gen_signal.cpp
@@ -58,84 +58,88 @@ TrajGenSignal::operator TrajGenMsg() const
     TrajGenMsg msg;
 
     int i = 0;
-    for(auto &foot_pair : this->foot_pos)
+    for(auto &leg_pair : this->foot_pos)
 	{
-	 	msg.foot_pos()[i*3] = this->foot_pos[foot_pair.key_][0];
-        msg.foot_pos()[i*3 + 1] = this->foot_pos[foot_pair.key_][1];
-        msg.foot_pos()[i*3 + 2] = this->foot_pos[foot_pair.key_][2];
+	 	msg.foot_pos()[i*3] = this->foot_pos[leg_pair.key_][0];
+        msg.foot_pos()[i*3 + 1] = this->foot_pos[leg_pair.key_][1];
+        msg.foot_pos()[i*3 + 2] = this->foot_pos[leg_pair.key_][2];
 
-        msg.foot_pos_HF()[i*3] = this->foot_pos_HF[foot_pair.key_][0];
-        msg.foot_pos_HF()[i*3 + 1] = this->foot_pos_HF[foot_pair.key_][1];
-        msg.foot_pos_HF()[i*3 + 2] = this->foot_pos_HF[foot_pair.key_][2];
+        msg.foot_pos_HF()[i*3] = this->foot_pos_HF[leg_pair.key_][0];
+        msg.foot_pos_HF()[i*3 + 1] = this->foot_pos_HF[leg_pair.key_][1];
+        msg.foot_pos_HF()[i*3 + 2] = this->foot_pos_HF[leg_pair.key_][2];
 
-        msg.foot_vel()[i*3] = this->foot_vel[foot_pair.key_][0];
-        msg.foot_vel()[i*3 + 1] = this->foot_vel[foot_pair.key_][1];
-        msg.foot_vel()[i*3 + 2] = this->foot_vel[foot_pair.key_][2];
+        msg.foot_vel()[i*3] = this->foot_vel[leg_pair.key_][0];
+        msg.foot_vel()[i*3 + 1] = this->foot_vel[leg_pair.key_][1];
+        msg.foot_vel()[i*3 + 2] = this->foot_vel[leg_pair.key_][2];
 
-        msg.foot_vel_HF()[i*3] = this->foot_vel_HF[foot_pair.key_][0];
-        msg.foot_vel_HF()[i*3 + 1] = this->foot_vel_HF[foot_pair.key_][1];
-        msg.foot_vel_HF()[i*3 + 2] = this->foot_vel_HF[foot_pair.key_][2];
+        msg.foot_vel_HF()[i*3] = this->foot_vel_HF[leg_pair.key_][0];
+        msg.foot_vel_HF()[i*3 + 1] = this->foot_vel_HF[leg_pair.key_][1];
+        msg.foot_vel_HF()[i*3 + 2] = this->foot_vel_HF[leg_pair.key_][2];
 
-        msg.foot_acc()[i*3] = this->foot_acc[foot_pair.key_][0];
-        msg.foot_acc()[i*3 + 1] = this->foot_acc[foot_pair.key_][1];
-        msg.foot_acc()[i*3 + 2] = this->foot_acc[foot_pair.key_][2];
+        msg.foot_acc()[i*3] = this->foot_acc[leg_pair.key_][0];
+        msg.foot_acc()[i*3 + 1] = this->foot_acc[leg_pair.key_][1];
+        msg.foot_acc()[i*3 + 2] = this->foot_acc[leg_pair.key_][2];
 
-        msg.nom_touch_down()[i*3] = this->nom_touch_down[foot_pair.key_][0];
-        msg.nom_touch_down()[i*3 + 1] = this->nom_touch_down[foot_pair.key_][1];
-        msg.nom_touch_down()[i*3 + 2] = this->nom_touch_down[foot_pair.key_][2];
+        msg.nom_touch_down()[i*3] = this->nom_touch_down[leg_pair.key_][0];
+        msg.nom_touch_down()[i*3 + 1] = this->nom_touch_down[leg_pair.key_][1];
+        msg.nom_touch_down()[i*3 + 2] = this->nom_touch_down[leg_pair.key_][2];
 
-        msg.touch_down()[i*3] = this->touch_down[foot_pair.key_][0];
-        msg.touch_down()[i*3 + 1] = this->touch_down[foot_pair.key_][1];
-        msg.touch_down()[i*3 + 2] = this->touch_down[foot_pair.key_][2];
+        msg.touch_down()[i*3] = this->touch_down[leg_pair.key_][0];
+        msg.touch_down()[i*3 + 1] = this->touch_down[leg_pair.key_][1];
+        msg.touch_down()[i*3 + 2] = this->touch_down[leg_pair.key_][2];
 
-        msg.swing_period()[i] = this->swing_period[foot_pair.key_];
+        msg.swing_period()[i] = this->swing_period[leg_pair.key_];
 
-        msg.stance()[i] = this->stance[foot_pair.key_];
+        msg.stance()[i] = this->stance[leg_pair.key_];
+
+        msg.normal_force_max()[i] = this->normal_force_max[leg_pair.key_];
+        msg.normal_force_min()[i] = this->normal_force_min[leg_pair.key_];
         i++;
     }
-    
     return msg;
 }
 
 TrajGenSignal &TrajGenSignal::operator= (TrajGenMsg &msg)
 {
     int i = 0;
-    for(auto &foot_pair : this->foot_pos)
+    for(auto &leg_pair : this->foot_pos)
 	{
-	 	this->foot_pos[foot_pair.key_][0] = msg.foot_pos()[i*3];
-        this->foot_pos[foot_pair.key_][1] = msg.foot_pos()[i*3 + 1];
-        this->foot_pos[foot_pair.key_][2] = msg.foot_pos()[i*3 + 2];
+	 	this->foot_pos[leg_pair.key_][0] = msg.foot_pos()[i*3];
+        this->foot_pos[leg_pair.key_][1] = msg.foot_pos()[i*3 + 1];
+        this->foot_pos[leg_pair.key_][2] = msg.foot_pos()[i*3 + 2];
 
-        this->foot_pos_HF[foot_pair.key_][0] = msg.foot_pos_HF()[i*3];
-        this->foot_pos_HF[foot_pair.key_][1] = msg.foot_pos_HF()[i*3 + 1];
-        this->foot_pos_HF[foot_pair.key_][2] = msg.foot_pos_HF()[i*3 + 2];
+        this->foot_pos_HF[leg_pair.key_][0] = msg.foot_pos_HF()[i*3];
+        this->foot_pos_HF[leg_pair.key_][1] = msg.foot_pos_HF()[i*3 + 1];
+        this->foot_pos_HF[leg_pair.key_][2] = msg.foot_pos_HF()[i*3 + 2];
 
-        this->foot_vel[foot_pair.key_][0] = msg.foot_vel()[i*3];
-        this->foot_vel[foot_pair.key_][1] = msg.foot_vel()[i*3 + 1];
-        this->foot_vel[foot_pair.key_][2] = msg.foot_vel()[i*3 + 2];
+        this->foot_vel[leg_pair.key_][0] = msg.foot_vel()[i*3];
+        this->foot_vel[leg_pair.key_][1] = msg.foot_vel()[i*3 + 1];
+        this->foot_vel[leg_pair.key_][2] = msg.foot_vel()[i*3 + 2];
 
-        this->foot_vel_HF[foot_pair.key_][0] = msg.foot_vel_HF()[i*3];
-        this->foot_vel_HF[foot_pair.key_][1] = msg.foot_vel_HF()[i*3 + 1];
-        this->foot_vel_HF[foot_pair.key_][2] = msg.foot_vel_HF()[i*3 + 2];
+        this->foot_vel_HF[leg_pair.key_][0] = msg.foot_vel_HF()[i*3];
+        this->foot_vel_HF[leg_pair.key_][1] = msg.foot_vel_HF()[i*3 + 1];
+        this->foot_vel_HF[leg_pair.key_][2] = msg.foot_vel_HF()[i*3 + 2];
 
-        this->foot_acc[foot_pair.key_][0] = msg.foot_acc()[i*3];
-        this->foot_acc[foot_pair.key_][1] = msg.foot_acc()[i*3 + 1];
-        this->foot_acc[foot_pair.key_][2] = msg.foot_acc()[i*3 + 2];
+        this->foot_acc[leg_pair.key_][0] = msg.foot_acc()[i*3];
+        this->foot_acc[leg_pair.key_][1] = msg.foot_acc()[i*3 + 1];
+        this->foot_acc[leg_pair.key_][2] = msg.foot_acc()[i*3 + 2];
 
-        this->nom_touch_down[foot_pair.key_][0] = msg.nom_touch_down()[i*3];
-        this->nom_touch_down[foot_pair.key_][1] = msg.nom_touch_down()[i*3 + 1];
-        this->nom_touch_down[foot_pair.key_][2] = msg.nom_touch_down()[i*3 + 2];
+        this->nom_touch_down[leg_pair.key_][0] = msg.nom_touch_down()[i*3];
+        this->nom_touch_down[leg_pair.key_][1] = msg.nom_touch_down()[i*3 + 1];
+        this->nom_touch_down[leg_pair.key_][2] = msg.nom_touch_down()[i*3 + 2];
 
-        this->touch_down[foot_pair.key_][0] = msg.touch_down()[i*3];
-        this->touch_down[foot_pair.key_][1] = msg.touch_down()[i*3 + 1];
-        this->touch_down[foot_pair.key_][2] = msg.touch_down()[i*3 + 2];
+        this->touch_down[leg_pair.key_][0] = msg.touch_down()[i*3];
+        this->touch_down[leg_pair.key_][1] = msg.touch_down()[i*3 + 1];
+        this->touch_down[leg_pair.key_][2] = msg.touch_down()[i*3 + 2];
 
-        this->swing_period[foot_pair.key_] = msg.swing_period()[i];
+        this->swing_period[leg_pair.key_] = msg.swing_period()[i];
 
-        this->stance[foot_pair.key_] = msg.stance()[i];
+        this->stance[leg_pair.key_] = msg.stance()[i];
+
+        this->normal_force_max[leg_pair.key_] = msg.normal_force_max()[i];
+        this->normal_force_min[leg_pair.key_] = msg.normal_force_min()[i];
         i++;
     }
-	
     return *this;
 }
 #endif // TRAJ_GEN_SIGNAL_CPP
