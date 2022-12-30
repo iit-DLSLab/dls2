64c490315b4cfb1e78eefc73a36a90f93a1e91b4
Marco Marchitto <marco.marchitto@iit.it>
2022-12-30 19:08:57 +0100

    adding trunk_controller_debug message

---
+++ modules/message_wrappers/include/dls2/msg_wrappers/debug/trunk_controller_debug.hpp
@@ -0,0 +1,44 @@
+/*******************************************************************************
+*                                                       ,----,                 *
+*                                                     .'   .' \                *
+*                                                   ,----,'    |               *
+*               ________  ___       ________        |    :  .  ;               *
+*              |\   ___ \|\  \     |\   ____\       ;    |.'  /                *
+*              \ \  \_|\ \ \  \    \ \  \___|_      `----'/  ;                 *
+*               \ \  \ \\ \ \  \    \ \_____  \       /  ;  /                  *
+*                \ \  \_\\ \ \  \____\|____|\  \     ;  /  /-,                 *
+*                 \ \_______\ \_______\____\_\  \   /  /  /.`|                 *
+*                  \|_______|\|_______|\_________\./__;      :                 *
+*                                     \|_________||   :    .'                  *
+*                                                 ;   | .'                     *
+*                                                 `---'                        *
+*******************************************************************************/
+#ifndef TRUNK_CONTROLLER_DEBUG_HPP
+#define TRUNK_CONTROLLER_DEBUG_HPP
+
+
+#include "robotlib/robot_base.hpp"
+
+#include "dls2/msg_wrappers/wrapper.hpp"
+#include "dls_messages/dds/debug_trunk_controller.h"
+
+namespace dls
+{
+	class TrunkControllerDebug : public Wrapper<TrunkControllerDebugMsg>
+	{
+	public:
+		TrunkControllerDebug(const std::shared_ptr<robotlib::RobotBase>);
+		TrunkControllerDebug(TrunkControllerDebug&);
+		TrunkControllerDebug() = delete;
+        ~TrunkControllerDebug();
+
+		operator TrunkControllerDebugMsg() const override;
+		TrunkControllerDebug& operator= (TrunkControllerDebugMsg&) override;
+
+		robotlib::LegDataMap<Eigen::Vector3d> feet_forces;
+		robotlib::LegDataMap<Eigen::Vector3d> desired_forces;
+		Eigen::Matrix<double,6,1> desired_wrench;
+	};
+} // end namespace dls
+
+#endif /* end of include guard: TRUNK_CONTROLLER_DEBUG_HPP */
