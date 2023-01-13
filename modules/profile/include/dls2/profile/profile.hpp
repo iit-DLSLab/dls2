8a92762bcfa078b10fcbbf5b24f28df653e85b79
Douglas Bertol <douglas.bertol@iit.it>
2023-01-13 11:28:54 +0100

    added prototypes for the utilization of profiles in dls2

---
+++ modules/profile/include/dls2/profile/profile.hpp
@@ -0,0 +1,83 @@
+/*******************************************************************************
+*                                                       ,----,                 *
+*                                                     .'   .' \                *
+*                                                   ,----,'    |               *
+*               ________  ___       ________        |    :  .  ;               *
+*              |\   ___ \|\  \     |\   ____\       ;    |.'  /                *
+*              \ \  \_|\ \ \  \    \ \  \___|_      `----'/  ;                 *
+*               \ \  \ \\ \ \  \    \ \_____  \       /  ;  /                  *
+*                \ \  \_\\ \ \  \____\|____|\  \     ;  /  /-,                 *
+*                 \ \_______\ \_______\____\_\  \   /  /  /.`|                 *
+*                  \|_______|\|_______|\_________\./__;      :                 *
+*                                     \|_________||   :    .'                  *
+*                                                 ;   | .'                     *
+*                                                 `---'                        *
+********************************************************************************
+* Author:            Hendrik de Bruin                                          *
+* Maintainer:        Hendrik de Bruin                                          *
+* author email:      hendrik.debruin@iit.it                                    *
+*******************************************************************************/
+#ifndef PROFILE_HPP_S1TMXWDT
+#define PROFILE_HPP_S1TMXWDT
+
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+
+namespace dls
+{
+	struct ProfileAction
+	{
+		enum class Action
+		{
+			/// Change the current profile to a specified profile
+			///
+			CHANGE_PROFILE,
+		};
+
+		std::vector<std::pair<Action, std::string>> actions;
+	};
+
+	class Profile
+	{
+	public:
+		/// Builds the Profile from a YAML string. The constructor will through
+		/// a std::runtime_error if the YAML string is malformated
+		/// @param yaml_config a YAML string
+		Profile(const std::string &yaml_config);
+
+		/// Query what action must be taken when a component crashes
+		///
+		/// This function does not actually take this action. It is up to the
+		/// layer to implement the correct action
+		///
+		/// @param component the name of the component that crashed. This
+		/// includes controllers, gait generators and state estimators
+		/// @return an Action
+		ProfileAction onCrash(const std::string &component);
+
+		/// The gait generators that is required to run this profile
+		///
+		std::string required_gait_generator;
+
+		/// The list of controllers that are required to run this profile
+		///
+		std::set<std::string> required_controllers;
+
+		/// The list of estimators that are required to run this profile
+		///
+		std::set<std::string> required_estimators;
+
+		/// List of the required states of the robot in order to run this
+		/// profile
+		std::set<std::string> required_states;
+
+		/// List of actions to take when a component crashes
+		///
+		/// maps component-name -> Action
+		std::map<std::string, ProfileAction> actions;
+	};
+} // end namespace dls
+
+#endif /* end of include guard: PROFILE_HPP_S1TMXWDT */
