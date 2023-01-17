18ac8271505941ddafcff9d757e1926adfe45596
Douglas Bertol <douglas.bertol@iit.it>
2023-01-17 13:10:45 +0100

    update visualization of the model, commit to close US26

--- modules/foxglove/src/foxserver.cpp
+++ modules/foxglove/src/foxserver.cpp
@@ -53,108 +53,6 @@ void FoxServer::on_topic_discovery(const std::string& topic_name, const std::str
 {
     std::cout << "Topic discovered: " << topic_name << " [ " << type_name << " ]" << std::endl;
 
-    if(topic_name == "blind_state")
-    {
-        // Create frames
-        std::ifstream jsonFrameSchemaFile("/usr/lib/dls2/dls_foxglove/FrameTransform.json");
-        json jsonFrameSchema = json::parse(jsonFrameSchemaFile);
-
-        const auto chanFrame = this->foxserver.addChannel({
-            "frames",
-            "json",
-            jsonFrameSchema["title"],
-            jsonFrameSchema.dump()
-        });
-
-        this->timer_flags.insert(chanFrame);
-
-        // Update scene
-        std::ifstream jsonSceneSchemaFile("/usr/lib/dls2/dls_foxglove/SceneUpdate.json");
-        json jsonSceneSchema = json::parse(jsonSceneSchemaFile);
-
-        auto chanScene = this->foxserver.addChannel({
-            "scene",
-            "json",
-            jsonSceneSchema["title"],
-            jsonSceneSchema.dump()
-        });
-
-        // Handler for connection
-        foxserver.setSubscribeHandler([&, this](ChannelId chanId) {
-            this->send_flags.erase(chanId);
-        });
-
-        this->ddslink.addReader("blind_state_foxglove",
-		dls::topics::low_level_estimation::blind_state,
-		std::function<void(void *)>
-		{
-            [&, chanFrame, chanScene](void *tuple)
-			{
-                auto epoch = nanosecondsSinceEpoch();
-                BlindStateMsg msg = *((BlindStateMsg*) tuple);
-
-                // if(this->send_flags.find(chanScene) == this->send_flags.end())
-                // {
-                //     std::ifstream jsonRobotFile("/usr/include/" + msg.robot_name() + "_description/foxglove/" + msg.robot_name() + ".json");
-                //     // std::ifstream jsonRobotFile("/home/dwbertol/dls2_ws/dls2_deploy/dls2/modules/foxglove/json/" + msg.robot_name() + ".json");
-
-                //     foxserver.sendMessage(chanScene, nanosecondsSinceEpoch(), json::parse(jsonRobotFile).dump());
-                //     this->send_flags.insert(chanScene);
-                // }
-
-        //         if(this->send_flags.find(chanFrame) != this->send_flags.end()) 
-        //             return;
-          
-        //         std::ifstream jsonFramesFile("/usr/include/" + msg.robot_name() + "_description/foxglove/" + msg.robot_name() + "_frames.json");
-        //         // std::ifstream jsonFramesFile("/home/dwbertol/dls2_ws/dls2_deploy/dls2/modules/foxglove/json/" + msg.robot_name() + "_frames.json");
-        //         json jsonFramesMsg = json::parse(jsonFramesFile);
-                
-        //         for(auto frame : jsonFramesMsg["transforms"])
-        //         {
-        //             if(frame["child_frame_id"] == "trunk")
-        //             {
-        //                 frame["translation"]["x"] = msg.base_pos_world()[0];
-        //                 frame["translation"]["y"] = msg.base_pos_world()[1];
-        //                 frame["translation"]["z"] = msg.base_pos_world()[2];
-
-        //                 frame["rotation"]["x"] = msg.base_ori_world()[0];
-        //                 frame["rotation"]["y"] = msg.base_ori_world()[1];
-        //                 frame["rotation"]["z"] = msg.base_ori_world()[2];
-        //                 frame["rotation"]["w"] = msg.base_ori_world()[3];
-        //             }
-        //             else
-        //             {
-        //                 int i = 0;
-        //                 for(auto joint_name : msg.joint_name())
-        //                 {
-        //                     if(joint_name == frame["child_frame_id"])
-        //                     {
-        //                         double a = frame["rotation"]["w"].get<double>();
-        //                         double b = frame["rotation"]["x"].get<double>();
-        //                         double c = frame["rotation"]["y"].get<double>();
-        //                         double d = frame["rotation"]["z"].get<double>();
-
-        //                         double e = cos(msg.joint_pos()[i]/2);
-        //                         double f = 0;
-        //                         double g = 0;
-        //                         double h = sin(msg.joint_pos()[i]/2);
-
-        //                         frame["rotation"]["x"] = b*e + a*f + c*h - d*g;
-        //                         frame["rotation"]["y"] = a*g - b*h + c*e + d*f;
-        //                         frame["rotation"]["z"] = a*h + b*g - c*f + d*e;
-        //                         frame["rotation"]["w"] = a*e - b*f - c*g - d*h;
-        //                         break;
-        //                     }
-        //                     i++;
-        //                 }
-        //             }
-        //             foxserver.sendMessage(chanFrame, epoch, frame.dump());
-        //         }
-        //         this->send_flags.insert(chanFrame);
-			}
-		});
-    }
-
     eprosima::fastrtps::types::DynamicType_ptr type_ = dds::get_type_registered_(type_name);
 
     auto jsonPair = dds::createJsonSchema(topic_name, type_);
@@ -185,4 +83,112 @@ void FoxServer::on_topic_discovery(const std::string& topic_name, const std::str
                 this->send_flags.insert(channel);
             }}
     );
+
+    if(topic_name == "blind_state")
+    {
+        // Create frames
+        std::ifstream jsonFrameSchemaFile("/usr/lib/dls2/dls_foxglove/FrameTransform.json");
+        json jsonFrameSchema = json::parse(jsonFrameSchemaFile);
+
+        const auto chanFrame = this->foxserver.addChannel({
+            "frames",
+            "json",
+            jsonFrameSchema["title"],
+            jsonFrameSchema.dump()
+        });
+
+        this->timer_flags.insert(chanFrame);
+
+        // Update scene
+        std::ifstream jsonSceneSchemaFile("/usr/lib/dls2/dls_foxglove/SceneUpdate.json");
+        json jsonSceneSchema = json::parse(jsonSceneSchemaFile);
+
+        auto chanScene = this->foxserver.addChannel({
+            "scene",
+            "json",
+            jsonSceneSchema["title"],
+            jsonSceneSchema.dump()
+        });
+
+        // this->timer_flags.insert(chanScene);
+
+        // Handler for connection
+        foxserver.setSubscribeHandler([&, this](ChannelId chanId) {
+            this->send_flags.erase(chanId);
+        });
+
+        this->ddslink.addReader("blind_state_foxglove",
+		dls::topics::low_level_estimation::blind_state,
+		std::function<void(void *)>
+		{
+            [&, chanFrame, chanScene, jsonPair, type_](void *tuple)
+			{
+
+                nlohmann::json jsonMsg = jsonPair.second;
+
+                dds::getDataToJson(topic_name, type_, (eprosima::fastrtps::types::DynamicData*) tuple, jsonMsg);
+
+                if(this->send_flags.find(chanScene) == this->send_flags.end())
+                {
+                    std::ifstream jsonRobotFile("/usr/include/" + jsonMsg["robot_name"].get<std::string>() + "_description/foxglove/" + jsonMsg["robot_name"].get<std::string>() + ".json");
+
+                    foxserver.sendMessage(chanScene, nanosecondsSinceEpoch(), json::parse(jsonRobotFile).dump());
+                    this->send_flags.insert(chanScene);
+                }
+
+                if(this->send_flags.find(chanFrame) != this->send_flags.end()) 
+                    return;
+
+                auto epoch = nanosecondsSinceEpoch();
+          
+                std::ifstream jsonFramesFile("/usr/include/" + jsonMsg["robot_name"].get<std::string>() + "_description/foxglove/" + jsonMsg["robot_name"].get<std::string>() + "_frames.json");
+
+                json jsonFramesMsg = json::parse(jsonFramesFile);
+                
+                for(auto frame : jsonFramesMsg["transforms"])
+                {
+                    if(frame["child_frame_id"] == "trunk")
+                    {
+                        frame["translation"]["x"] = jsonMsg["base_pos_world"][0];
+                        frame["translation"]["y"] = jsonMsg["base_pos_world"][1];
+                        frame["translation"]["z"] = jsonMsg["base_pos_world"][2];
+
+                        frame["rotation"]["x"] = jsonMsg["base_ori_world"][0];
+                        frame["rotation"]["y"] = jsonMsg["base_ori_world"][1];
+                        frame["rotation"]["z"] = jsonMsg["base_ori_world"][2];
+                        frame["rotation"]["w"] = jsonMsg["base_ori_world"][3];
+                    }
+                    else
+                    {
+                        int i = 0;
+                        for(auto joint_name : jsonMsg["joint_name"])
+                        {
+                            if(joint_name == frame["child_frame_id"])
+                            {
+                                double a = frame["rotation"]["w"].get<double>();
+                                double b = frame["rotation"]["x"].get<double>();
+                                double c = frame["rotation"]["y"].get<double>();
+                                double d = frame["rotation"]["z"].get<double>();
+
+                                double e = cos(jsonMsg["joint_pos"][i].get<double>()/2);
+                                double f = 0;
+                                double g = 0;
+                                double h = sin(jsonMsg["joint_pos"][i].get<double>()/2);
+
+                                frame["rotation"]["x"] = b*e + a*f + c*h - d*g;
+                                frame["rotation"]["y"] = a*g - b*h + c*e + d*f;
+                                frame["rotation"]["z"] = a*h + b*g - c*f + d*e;
+                                frame["rotation"]["w"] = a*e - b*f - c*g - d*h;
+                                break;
+                            }
+                            i++;
+                        }
+                    }
+                    foxserver.sendMessage(chanFrame, epoch, frame.dump());
+                }
+                this->send_flags.insert(chanFrame);
+			}
+		});
+    }
+
 }
