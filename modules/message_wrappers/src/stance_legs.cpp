2c0e118f88c55bd8efe946a9fe114285954079a5
Marco Marchitto <marco.marchitto@iit.it>
2023-01-17 10:48:53 +0100

    added stance_legs wrapper

---
+++ modules/message_wrappers/src/stance_legs.cpp
@@ -0,0 +1,73 @@
+/*******************************************************************************
+*                                                       ,----,                 *
+*                                                     .'   .' \                *
+*                                                   ,----,'    |               *
+*               ________  ___       ________        |    :  .  ;               *
+*              |\   ___ \|\  \     |\   ____\       ;    |.'  /                *
+*              \ \  \_|\ \ \  \    \ \  \___|_      `----'/  ;                 *
+*               \ \  \ \\ \ \  \    \ \_____  \       /  ;  /                  *
+*                \ \  \_\\ \ \  \____\|____|\  \     ;  /  /-,                 *
+*                 \ \_______\ \_______\____\_\  \   /  /  /.`|                 *
+*                  \|_______|\|_______|\_________\./__;      :                 *
+*                                     \|_________||   :    .'                  *
+*                                                 ;   | .'                     *
+*                                                 `---'                        *
+*******************************************************************************/
+
+#ifndef STANCE_LEGS_CPP
+#define STANCE_LEGS_CPP
+
+#include "dls2/msg_wrappers/stance_legs.hpp"
+
+using namespace dls;
+
+StanceLegs::StanceLegs(const std::shared_ptr<robotlib::RobotBase> pRobot)
+	: robot_name("")
+	, stance_legs(pRobot->makeLegDataMap<bool>(false))
+	, time(0)
+{ }
+
+StanceLegs::StanceLegs(StanceLegs& from)
+	: robot_name(from.robot_name)
+	, stance_legs(from.stance_legs)
+	, time(from.time)
+{ }
+
+StanceLegs::~StanceLegs()
+{ }
+
+StanceLegs::operator StanceLegsMsg() const
+{
+    StanceLegsMsg msg;
+
+	msg.robot_name() = this->robot_name;
+
+	int leg_id = 0;
+	for(auto &leg : this->stance_legs)
+	{
+		msg.stance_legs()[leg_id] = this->stance_legs[leg.key_];
+		leg_id++;
+	}
+
+	msg.time(this->time);
+
+    return msg;
+}
+
+StanceLegs& StanceLegs::operator= (StanceLegsMsg& msg)
+{
+	this->robot_name = msg.robot_name();
+
+	int leg_id = 0;
+	for(auto &leg : this->stance_legs)
+	{
+		this->stance_legs[leg.key_] = msg.stance_legs()[leg_id];
+		leg_id++;
+	}
+
+	this->time = msg.time();
+
+	return *this;
+}
+
+#endif // STANCE_LEGS_CPP
