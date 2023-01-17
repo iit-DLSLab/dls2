7f14a05834ca2dde488d70cac65b41bc0793265e
Marco Marchitto <marco.marchitto@iit.it>
2023-01-17 10:44:56 +0100

    adding base_state wrapper

---
+++ modules/message_wrappers/include/dls2/msg_wrappers/base_state.hpp
@@ -0,0 +1,48 @@
+/*******************************************************************************
+*                                                       ,----,                 *
+*                                                     .'   .' \                *
+*                                                   ,----,'    |               *
+*               ________  ___       ________        |    :  .  ;               *
+*              |\   ___ \|\  \     |\   ____\       ;    |.'  /                *
+*              \ \  \_|\ \ \  \    \ \  \___|_      `----'/  ;                 *
+*               \ \  \ \\ \ \  \    \ \_____  \       /  ;  /                  *
+*                \ \  \_\\ \ \  \____\|____|\  \     ;  /  /-,                 *
+*                 \ \_______\ \_______\____\_\  \   /  /  /.`|                 *
+*                  \|_______|\|_______|\_________\./__;      :                 *
+*                                     \|_________||   :    .'                  *
+*                                                 ;   | .'                     *
+*                                                 `---'                        *
+*******************************************************************************/
+#ifndef BASE_STATE_HPP
+#define BASE_STATE_HPP
+
+#include "robotlib/robot_base.hpp"
+
+#include "dls2/msg_wrappers/wrapper.hpp"
+#include "dls2/msg_wrappers/pose.hpp"
+#include "dls2/msg_wrappers/screw.hpp"
+#include "dls_messages/dds/base_state.h"
+
+namespace dls
+{
+	class BaseState : public Wrapper<BaseStateMsg>
+	{
+	public:
+		BaseState();
+		BaseState(BaseState&);
+        ~BaseState();
+
+		operator BaseStateMsg() const override;
+		BaseState& operator= (BaseStateMsg&) override;
+
+		std::string robot_name;
+
+		dls::Pose pose;
+		dls::Screw velocity;
+		dls::Screw acceleration;
+
+		unsigned long long time;
+	};
+} // end namespace dls
+
+#endif /* end of include guard: BASE_STATE_HPP */
