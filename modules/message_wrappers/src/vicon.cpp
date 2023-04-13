a9a1730b38738c6ddc8c40bf2b870596f6013620
GianlucaCerilli <gianluca.cerilli@iit.it>
2023-04-13 17:31:32 +0200

    commented the usage of PointMsg for markers publications with EDIT#
    the usage of custom msg does not work in the current version of Plotjuggler with FastDDS

--- modules/message_wrappers/src/vicon.cpp
+++ modules/message_wrappers/src/vicon.cpp
@@ -45,15 +45,21 @@ namespace dls
         msg.robot_orientation()[2] = this->robot_orientation.z();
         msg.robot_orientation()[3] = this->robot_orientation.w();
 
+        // EDIT#4/7 use it when PlotJuggler plugin for FastDDS supports sequence data structures
+        //for(unsigned int marker_index{0}; marker_index < this->markers_positions.size(); ++marker_index)
+        //{
+        //    PointMsg marker;
+        //
+        //    marker.x() = this->markers_positions.at(marker_index)[0];
+        //    marker.y() = this->markers_positions.at(marker_index)[1];
+        //    marker.z() = this->markers_positions.at(marker_index)[2];
+        //
+        //    msg.markers_positions().push_back(marker);
+        //}
+
         for(unsigned int marker_index{0}; marker_index < this->markers_positions.size(); ++marker_index)
         {
-            PointMsg marker;
-
-            marker.x() = this->markers_positions.at(marker_index)[0]; 
-            marker.y() = this->markers_positions.at(marker_index)[1]; 
-            marker.z() = this->markers_positions.at(marker_index)[2]; 
-
-            msg.markers_positions().push_back(marker);
+            msg.markers_positions().push_back(this->markers_positions.at(marker_index));
         }
 
         return msg;
@@ -72,13 +78,19 @@ namespace dls
         this->robot_orientation.z() = msg.robot_orientation()[2];
         this->robot_orientation.w() = msg.robot_orientation()[3];
 
+        // EDIT#5/7 use it when PlotJuggler plugin for FastDDS supports sequence data structures
+        //for(unsigned int marker_index{0}; marker_index < msg.markers_positions().size(); ++marker_index)
+        //{
+        //    Eigen::Vector3d marker{msg.markers_positions().at(marker_index).x(),
+        //                           msg.markers_positions().at(marker_index).y(),
+        //                           msg.markers_positions().at(marker_index).z()};
+        //
+        //    this->markers_positions.push_back(marker);
+        //}
+
         for(unsigned int marker_index{0}; marker_index < msg.markers_positions().size(); ++marker_index)
         {
-            Eigen::Vector3d marker{msg.markers_positions().at(marker_index).x(),
-                                   msg.markers_positions().at(marker_index).y(),
-                                   msg.markers_positions().at(marker_index).z()};
-
-            this->markers_positions.push_back(marker);
+            this->markers_positions.push_back(msg.markers_positions().at(marker_index));
         }
 
         return *this;
@@ -91,11 +103,17 @@ namespace dls
         this->robot_position = from.robot_position;
         this->robot_orientation = from.robot_orientation;
 
+        // EDIT#6/7 use it when PlotJuggler plugin for FastDDS supports sequence data structures
+        //for(unsigned int marker_index{0}; marker_index < from.markers_positions.size(); ++marker_index)
+        //{
+        //    this->markers_positions.at(marker_index)[0] = from.markers_positions.at(marker_index)[0];
+        //    this->markers_positions.at(marker_index)[1] = from.markers_positions.at(marker_index)[1];
+        //    this->markers_positions.at(marker_index)[2] = from.markers_positions.at(marker_index)[2];
+        //}
+
         for(unsigned int marker_index{0}; marker_index < from.markers_positions.size(); ++marker_index)
         {
-            this->markers_positions.at(marker_index)[0] = from.markers_positions.at(marker_index)[0];
-            this->markers_positions.at(marker_index)[1] = from.markers_positions.at(marker_index)[1];
-            this->markers_positions.at(marker_index)[2] = from.markers_positions.at(marker_index)[2];
+            this->markers_positions.at(marker_index) = from.markers_positions.at(marker_index);
         }
 
         return *this;
