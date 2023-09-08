0f0dd293542b7ead4fabf2300240100c7271a853
Gianluca <gianluca.cerilli@iit.it>
2023-09-08 15:25:25 +0200

    console layer refactoring and autocompletion
    - refactoring for a more readable code and removed unused structures and functions
    - [WIP] added command_completion function
    - [WIP] added command_generator function
    	- added basic readline implementation
    	- added commands history
    - changed list command to show commands for all the running processes (or just for a specific one)
    - renamed cleanZombies

--- modules/core_framework/include/dls2/core_framework/console_layer.hpp
+++ modules/core_framework/include/dls2/core_framework/console_layer.hpp
@@ -1,62 +1,27 @@
 
-#ifndef CONSOLE_LAYER_HPP_BXNRZS8Q
-#define CONSOLE_LAYER_HPP_BXNRZS8Q
+#ifndef CONSOLE_LAYER_HPP
+#define CONSOLE_LAYER_HPP
 
-// framework core
 #include "dls2/application/layer.hpp"
-
-#include <signal.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
 namespace dls
 {
-	/// A console interface into the framework
-	///
 	class ConsoleLayer : public Layer
 	{
-		// Console completion is handled by readline, which is a C-library.
-		// Therefore, need to declare this as a friend
-		friend char *command_completion(const char *text, int state);
-		friend char **console_completion(const char *text, int start, int end);
-		friend char *arg_completion(const char * text, int state);
-
 	public:
-		/// Default Constructor
-		///
 		ConsoleLayer(std::string ID);
+		virtual ~ConsoleLayer();
 
-		/// Default Destructor
-		///
-		~ConsoleLayer();
+		// Functions for autocompletion with "Tab" key, implemented using the GNU Readline library
+		static char **command_completion(const char *text, int start, int end);
+		static char *command_generator(const char *text, int state);
 
-		/// Run the console
-		///
-		/// This is a blocking call that enters an infinite loop
 		AppStatus run() override;
-
-		/// Stop the console
-		///
-		/// Will cause `run` to stop running
 		AppStatus stop() override;
-
-		/// Print the state of this layer
-		///
-		/// prints:
-		/// * active controllers
-		/// * active motion generator
-		/// * published torque
-		std::string where() override{return "not yet implemented";}
-
-	private:
-		/// Generates the console prompt
-		///
-		/// This is currently just a stub function, but it can be expanded apon in
-		/// future
-		std::string build_prompt();
-
-		/// Handler to the shutdown SIGINT (ctrl+c) signal
-		/// 
-		static void handle_signals(int);
+		std::string where() override;
 	};
-} // end namespace dls
+} // namespace dls
 
-#endif /* end of include guard: CONSOLE_LAYER_HPP_BXNRZS8Q */
+#endif
