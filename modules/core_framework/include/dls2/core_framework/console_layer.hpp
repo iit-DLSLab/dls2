9e51f4731b17fa234245311a6a8a54de37e87842
Gianluca <gianluca.cerilli@iit.it>
2023-09-21 12:05:44 +0200

    console layer refactoring and autocompletion [PT2]
    - added readline functions for Tab completion as ConsoleLayer friends
    - added utility functions to get processes and commands
    - added utilitu functions to show processes and commands
    - added comments all over the code
    - added reset command
    - changed list command
    - removed old structures
    - implemented:
    	- ">" string for line input
    	- tab completion from empty window
    	- tab completion from process
    	- tab completion from command
    	- handling of unavailable or ambiguous commands
    
    - [WIP] added utility functions to show load[Layer] possible arguments (not possible to tab them yet)
    - [WIP] possibility to tab load[Layer] arguments
    - [WIP] If process is matched but has multiple commands matched, show only these, no all its commands

--- modules/core_framework/include/dls2/core_framework/console_layer.hpp
+++ modules/core_framework/include/dls2/core_framework/console_layer.hpp
@@ -5,22 +5,55 @@
 #include "dls2/application/layer.hpp"
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <filesystem>
 
 namespace dls
 {
 	class ConsoleLayer : public Layer
 	{
+		// Functions for autocompletion with "Tab" key, implemented using the GNU Readline library
+		// Since these functions are part of GNU Readline library, they could be static (but in that case
+		// they could not access DLS2 member variables), so we make them friend of dls::ConsoleLayer class
+		// (we then need forward declaration and an additional ConsoleLayer object visible from them).
+
+		// Generate possible matches corresponding to the partial input entered by the user.
+		// "start" and "end" are indices in rl_line_buffer defining the boundaries of "text"
+		friend char **command_completion(const char *text, int start, int end);
+
+		// Takes the partial input entered by the user and a state as arguments.
+		// The "state" is zero the first time the function is called for a partial input and non-zero otherwise.
+		friend char *command_generator(const char *text, int state);
+
+		// Display the list of possible matches when "Tab" key is pressed.
+		// "matches" is the array of matching strings, "num_matches" is the number of strings in that array and 
+		// "max_length" is the length of the longest string in that array
+		friend void display_matches(char **matches, int num_matches, int max_length);
+
 	public:
 		ConsoleLayer(std::string ID);
 		virtual ~ConsoleLayer();
 
-		// Functions for autocompletion with "Tab" key, implemented using the GNU Readline library
-		static char **command_completion(const char *text, int start, int end);
-		static char *command_generator(const char *text, int state);
+		// Return all the available commands sorted by name, or just the ones of specific processes
+		const std::map<std::string, std::set<std::string>> getProcessesAndCommands(const std::set<std::string>& matches = std::set<std::string>({}));
+
+		// Return the "load_layers_paths" map with "load[Layer]" commands and associated installation folder
+		const std::map<std::string, std::string> getLoadLayersPaths();
+
+		// "matches" can contain both processes and commands. Show:
+		// - all the available commands [if the set is empty]
+		// - all the available commands of a specific process [if the set element is a process]
+		// - the process names with the command specified [if the set element is a command available under different processes]
+		void printProcessesAndCommands(const std::set<std::string>& matches = std::set<std::string>({}));
+
+		// Show the possible arguments for "load[Layer]" commands
+		void printLoadArguments(const std::string& load_command);
 
 		AppStatus run() override;
 		AppStatus stop() override;
 		std::string where() override;
+
+	private:
+		const std::map<std::string, std::string> load_layers_paths_;
 	};
 } // namespace dls
 
