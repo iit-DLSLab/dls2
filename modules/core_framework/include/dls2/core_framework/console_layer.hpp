e3885d41d89fdcd5c1bbe66af8dd9cb96d8f1ec3
Gianluca <gianluca.cerilli@iit.it>
2023-10-27 10:22:11 +0200

    console layer refactoring and autocompletion [PT4]
    - If a process has multiple commands that can match, only these are shown, not all its available commands
    - Simplified the display function and removed an useless print method
    - Removed possibility of 'tab' in case of blank spaces at the beginning of the line
    - Cleaned variables, updated all the comments code and removed old/useless parts
    
    - [WIP] Edit the 'DLS' paths for load[Layer] commands (not all are available or contains proper files)

--- modules/core_framework/include/dls2/core_framework/console_layer.hpp
+++ modules/core_framework/include/dls2/core_framework/console_layer.hpp
@@ -11,22 +11,24 @@ namespace dls
 {
 	class ConsoleLayer : public Layer
 	{
-		// Functions for autocompletion with "Tab" key, implemented using the GNU Readline library
-		// Since these functions are part of GNU Readline library, they could be static (but in that case
-		// they could not access DLS2 member variables), so we make them friend of dls::ConsoleLayer class
-		// (we then need forward declaration and an additional ConsoleLayer object visible from them).
+		// Functions for autocompletion with "Tab" key, implemented using the GNU Readline library.
+		// Since these functions are part of the GNU Readline library, they could be static (but in that case
+		// they could not access DLS2 member variables), so we make them friend of "dls::ConsoleLayer" class
+		// (we then need forward declaration and a "dls::readline_completion::ConsoleLayer" object visible from them).
 
 		// Generate possible matches corresponding to the partial input entered by the user.
-		// "start" and "end" are indices in rl_line_buffer defining the boundaries of "text"
+		// "start" and "end" are indices in "rl_line_buffer" defining the boundaries of "text"
 		friend char **command_completion(const char *text, int start, int end);
 
-		// Takes the partial input entered by the user and a state as arguments.
+		// Take the partial input entered by the user and a state as arguments.
 		// The "state" is zero the first time the function is called for a partial input and non-zero otherwise.
+		// It generates matches for commands in the form "process::command"
 		friend char *command_generator(const char *text, int state);
 
+		// Same logic of the "command_generator" function, but it generates matches for the "load[Layer]" arguments
 		friend char *load_layer_argument_generator(const char *text, int state);
 
-		// Display the list of possible matches when "Tab" key is pressed.
+		// Print the list of possible matches when "Tab" key is pressed.
 		// "matches" is the array of matching strings, "num_matches" is the number of strings in that array and 
 		// "max_length" is the length of the longest string in that array
 		friend void display_matches(char **matches, int num_matches, int max_length);
@@ -35,26 +37,21 @@ namespace dls
 		ConsoleLayer(std::string ID);
 		virtual ~ConsoleLayer();
 
-		// Return all the available commands sorted by name, or just the ones of specific processes
-		const std::map<std::string, std::set<std::string>> getProcessesAndCommands(const std::set<std::string>& matches = std::set<std::string>({}));
+		// Return the complete set of available processes and commands sorted by name, or just the processes associated to "command"
+		const std::map<std::string, std::set<std::string>> getProcessesAndCommands(const std::string& command = std::string());
 
-		// Return the "load_layers_paths" map with "load[Layer]" commands and associated installation folder
+		// Return the "load_layers_paths_" class variable
 		const std::map<std::string, std::string> getLoadLayersPaths();
 
-		// "matches" can contain both processes and commands. Show:
-		// - all the available commands [if the set is empty]
-		// - all the available commands of a specific process [if the set element is a process]
-		// - the process names with the command specified [if the set element is a command available under different processes]
-		void printProcessesAndCommands(const std::set<std::string>& matches = std::set<std::string>({}));
-
-		// Show the possible arguments for "load[Layer]" commands
-		void printLoadArguments(const std::string& load_command);
+		// Print the processes and their commands according to the values of "matches" (the matched processes and commands) and "command"
+		void printProcessesAndCommands(const std::string& command = std::string());
 
 		AppStatus run() override;
 		AppStatus stop() override;
 		std::string where() override;
 
 	private:
+		// Map with "load[Layer]" commands and associated installation folders
 		const std::map<std::string, std::string> load_layers_paths_;
 	};
 } // namespace dls
