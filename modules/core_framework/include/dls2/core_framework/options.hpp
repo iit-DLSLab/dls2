99ea1ef86971928f3deffe0e6a78d65b7451bb50
Marco Marchitto <marco.marchitto@iit.it>
2024-05-09 14:51:19 +0200

    you now need yo explicitly launching the servers with --server option
    But from now on you can start using dls --startup

--- modules/core_framework/include/dls2/core_framework/options.hpp
+++ modules/core_framework/include/dls2/core_framework/options.hpp
@@ -49,6 +49,7 @@ public:
 	static bool launch_service;
 	static bool launch_supervisor;
 	static bool run_startup;
+	static bool run_servers;
 	static bool kill_dls2;
 	static std::string startup_file;
 	static std::string estimation_layer_name;
