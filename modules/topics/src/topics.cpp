316d458b0f490d049daf4f1e17a1536018ca9b9b
sven1904 <sven.tittel@iit.it>
2025-11-28 13:29:24 +0100

    message/topic updates for ros2; removed unused messages;

--- modules/topics/src/topics.cpp
+++ modules/topics/src/topics.cpp
@@ -7,81 +7,90 @@
 namespace dls
 {
 
-	namespace topics
-	{
-		// logs
-		dls::topicType warn_log_stream = dls::topicType("warn_log_stream", new dls2_interface::msg::StringMsgPubSubType());
-		dls::topicType info_log_stream = dls::topicType("info_log_stream", new dls2_interface::msg::StringMsgPubSubType());
-		dls::topicType error_log_stream = dls::topicType("error_log_stream", new dls2_interface::msg::StringMsgPubSubType());
-		dls::topicType fatal_log_stream = dls::topicType("fatal_log_stream", new dls2_interface::msg::StringMsgPubSubType());
-		dls::topicType debug_log_stream = dls::topicType("debug_log_stream", new dls2_interface::msg::StringMsgPubSubType());
-		dls::topicType log_events = dls::topicType("log_events", new dls2_interface::msg::EventLogPubSubType());
+  namespace topics
+  {
+    // logs
+    dls::topicType warn_log_stream = dls::topicType("warn_log_stream",
+        new dls2_interface::msg::StringPubSubType());
+    dls::topicType info_log_stream = dls::topicType("info_log_stream",
+        new dls2_interface::msg::StringPubSubType());
+    dls::topicType error_log_stream = dls::topicType("error_log_stream",
+        new dls2_interface::msg::StringPubSubType());
+    dls::topicType fatal_log_stream = dls::topicType("fatal_log_stream",
+        new dls2_interface::msg::StringPubSubType());
+    dls::topicType debug_log_stream = dls::topicType("debug_log_stream",
+        new dls2_interface::msg::StringPubSubType());
+    dls::topicType log_events = dls::topicType("log_events",
+        new dls2_interface::msg::EventLogPubSubType());
 
-		// command
-		dls::topicType command_call = dls::topicType("command_call", new dls2_interface::msg::CommandCallPubSubType());
-		dls::topicType command_send = dls::topicType("command_send", new dls2_interface::msg::CommandSendPubSubType());
-		dls::topicType command_feedback = dls::topicType("command_feedback", new dls2_interface::msg::StringMsgPubSubType());
+    // command
+    dls::topicType command_call = dls::topicType("command_call",
+        new dls2_interface::msg::CommandCallPubSubType());
+    dls::topicType command_feedback = dls::topicType("command_feedback",
+        new dls2_interface::msg::StringPubSubType());
 
-		// simulation
-		dls::topicType time_factor = dls::topicType("time_factor", new dls2_interface::msg::DoubleMsgPubSubType());
+    // simulation
+    dls::topicType time_factor = dls::topicType("time_factor",
+        new dls2_interface::msg::DoublePubSubType());
 
-		// development pool
-		dls::topicType develop_testbench = dls::topicType("develop_testbench", new dls2_interface::msg::StringMsgPubSubType());
+    // development pool
+    dls::topicType develop_testbench = dls::topicType("develop_testbench",
+        new dls2_interface::msg::StringPubSubType());
 
-		// control signals
-		dls::topicType desired_torques = dls::topicType("desired_torques", new dls2_interface::msg::DesiredTorquesPubSubType());
-		dls::topicType control_signal = dls::topicType("control_signal", new dls2_interface::msg::ControlSignalPubSubType());
-		dls::topicType trajectory_generator = dls::topicType("trajectory_generator", new dls2_interface::msg::TrajectoryGeneratorPubSubType());
-		dls::topicType joy_signal = dls::topicType("joy_signal", new dls2_interface::msg::ControllerCommandPubSubType());
-		dls::topicType controller_signal = dls::topicType("controller_command", new dls2_interface::msg::ControllerCommandPubSubType());
-		dls::topicType mpc_generator_output = dls::topicType("mpc_generator_output", new dls2_interface::msg::MPCGeneratorOutputPubSubType());
+    // control signals
+    dls::topicType controller_command = dls::topicType("controller_command",
+        new dls2_interface::msg::ControllerCommandPubSubType());
+    dls::topicType desired_torques = dls::topicType("desired_torques",
+        new dls2_interface::msg::DesiredTorquesPubSubType());
+    dls::topicType control_signal = dls::topicType("control_signal",
+        new dls2_interface::msg::ControlSignalPubSubType());
+    dls::topicType trajectory_generator = dls::topicType("trajectory_generator",
+        new dls2_interface::msg::TrajectoryGeneratorPubSubType());
     dls::topicType navigation_reference = dls::topicType("navigation_reference",
         new dls2_interface::msg::NavigationReferencePubSubType());
     dls::topicType button_event = dls::topicType("button_event",
         new dls2_interface::msg::ButtonEventPubSubType());
-    dls::topicType operational_mode = dls::topicType("operational_mode",
-        new dls2_interface::msg::OperationalModePubSubType());
 
-		// services
-		dls::topicType param_server = dls::topicType("add_double", new dls2_interface::msg::ParamServerPubSubType());
+    // services
+    dls::topicType param_server = dls::topicType("add_double",
+        new dls2_interface::msg::ParamServerPubSubType());
 
-		// visualizations
+    // state machine
+    dls::topicType state_machine = dls::topicType("state_machine",
+        new dls2_interface::msg::StateMachineMonitorPubSubType());
 
-		dls::topicType sphere_list = dls::topicType("sphere_list", new dls2_interface::msg::SphereListPubSubType());
-		dls::topicType arrow_list = dls::topicType("arrow_list", new dls2_interface::msg::ArrowListPubSubType());
-		dls::topicType friction_cone_list = dls::topicType("friction_cone_list", new dls2_interface::msg::FrictionConeListPubSubType());
+    // arm controller
+    dls::topicType desired_arm_torques = dls::topicType("desired_arm_torques",
+        new dls2_interface::msg::DesiredArmTorquesPubSubType());
+    dls::topicType arm_trajectory_generator = dls::topicType("arm_trajectory_generator",
+        new dls2_interface::msg::ArmTrajectoryGeneratorPubSubType());
 
-		dls::topicType generic_robot = dls::topicType("generic_robot", new dls2_interface::msg::GenericRobotPubSubType());
+    namespace high_level_estimation
+    {
+      dls::topicType legs_pose = dls::topicType("legs_pose",
+          new dls2_interface::msg::LegsPosePubSubType());
+      dls::topicType attitude_estimation = dls::topicType("attitude_estimation",
+          new dls2_interface::msg::AttitudePubSubType());
+      dls::topicType sensor_fusion = dls::topicType("sensor_fusion",
+          new dls2_interface::msg::SensorFusionPubSubType());
+      dls::topicType vicon = dls::topicType("vicon",
+          new dls2_interface::msg::ViconPubSubType());
+      dls::topicType base_state = dls::topicType("base_state",
+          new dls2_interface::msg::BaseStatePubSubType());
+      dls::topicType stance_status = dls::topicType("stance_status",
+          new dls2_interface::msg::StanceStatusPubSubType());
+    }
 
-
-		// state machine
-
-		dls::topicType state_machine = dls::topicType("state_machine", new dls2_interface::msg::StateMachineMonitorPubSubType());
-
-		// arm controller
-		dls::topicType desired_arm_torques = dls::topicType("desired_arm_torques", new dls2_interface::msg::DesiredArmTorquesPubSubType());
-		dls::topicType arm_trajectory_generator = dls::topicType("arm_trajectory_generator", new dls2_interface::msg::ArmTrajectoryGeneratorPubSubType());
-		namespace high_level_estimation
-		{
-			dls::topicType legs_pose = dls::topicType("legs_pose", new dls2_interface::msg::LegsPosePubSubType());
-			dls::topicType t265_odometry = dls::topicType("t265_odometry", new dls2_interface::msg::T265OdometryPubSubType());
-			dls::topicType attitude_estimation = dls::topicType("attitude_estimation", new dls2_interface::msg::AttitudePubSubType());
-			dls::topicType sensor_fusion = dls::topicType("sensor_fusion", new dls2_interface::msg::SensorFusionPubSubType());
-			dls::topicType vicon = dls::topicType("vicon", new dls2_interface::msg::ViconPubSubType());
-			dls::topicType base_state = dls::topicType("base_state", new dls2_interface::msg::BaseStatePubSubType());
-			dls::topicType stance_status = dls::topicType("stance_status", new dls2_interface::msg::StanceStatusPubSubType());
-		}
-
-		namespace low_level_estimation
-		{
+    namespace low_level_estimation
+    {
       dls::topicType arm_state = dls::topicType("arm_state",
           new dls2_interface::msg::ArmStatePubSubType());
-			dls::topicType blind_state = dls::topicType("blind_state", new dls2_interface::msg::BlindStatePubSubType());
-			dls::topicType imu = dls::topicType("imu", new dls2_interface::msg::ImuPubSubType());
-			dls::topicType slip_flag = dls::topicType("slip_flag", new dls2_interface::msg::SlipFlagPubSubType());
-			dls::topicType arm_blind_state = dls::topicType("arm_blind_state", new dls2_interface::msg::ArmBlindStatePubSubType());
-		}
-	}
+      dls::topicType blind_state = dls::topicType("blind_state",
+          new dls2_interface::msg::BlindStatePubSubType());
+      dls::topicType imu = dls::topicType("imu",
+          new dls2_interface::msg::ImuPubSubType());
+    }
+  }
 }
 
 #endif /* end of include guard: TOPICS_CPP */
--- test/communication/writer.cpp
+++ test/communication/writer.cpp
@@ -1,31 +1,36 @@
 #include "dls2/util/messaging/dds_participant.hpp"
-#include "dls_messages/dds/BaseReferencesPubSubTypes.hpp"
+#include "dls_messages/dds/NavigationReferencePubSubTypes.hpp"
 
 #include <thread>
 #include <any>
 
 int main()
 {
-    dls::DDSParticipant participant("participant", 0);
-    auto writer = participant.addWriter("dds_writer", dls::topicType("Sample", new dls2_interface::msg::BaseReferencesPubSubType()));
-    dls2_interface::msg::BaseReferences msg;
-    msg.sequence_id() = 0;
-    msg.timestamp() = 1;
-    msg.base_reference()[0] = 0;
-    msg.base_reference()[1] = 1;
-    msg.base_reference()[2] = 2;
-    int count = 0;
-    while(true)
-    {
-        msg.sequence_id() = count;
-        msg.timestamp()  = count +1;
-        msg.base_reference()[0] = count + 0;
-        msg.base_reference()[1] = count + 1;
-        msg.base_reference()[2] = count + 2;
-        count++;
-        
-        writer->write(&msg);
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
-    }
-    return 0;
+  dls::DDSParticipant participant("participant", 0);
+  auto writer = participant.addWriter("dds_writer", dls::topicType("Sample",
+      new dls2_interface::msg::NavigationReferencePubSubType()));
+  dls2_interface::msg::NavigationReference msg;
+  msg.sequence_id() = 0;
+  msg.timestamp() = 0;
+  msg.velocity()[0] = 0;
+  msg.velocity()[1] = 0.2;
+  msg.velocity()[2] = 0.4;
+  msg.robot_height() = 0.8;
+  msg.start() = false;
+  int count = 0;
+  while(true)
+  {
+    msg.sequence_id() = count;
+    msg.timestamp()  = count * 0.5;
+    msg.velocity()[0] = count;
+    msg.velocity()[1] = count + 0.2;
+    msg.velocity()[2] = count + 0.4;
+    msg.robot_height() = count + 0.8;
+    msg.start() = count % 2 > 0;
+    count++;
+
+    writer->write(&msg);
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+  }
+  return 0;
 }
