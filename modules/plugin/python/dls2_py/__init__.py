a6850c64aeb48093ec786bf5733038c5f79d935d
mich-pest <michele.pestarino@outlook.it>
2026-05-22 14:17:03 +0200

    dls2_py: Plugin and PeriodicAppPlugin defined in python

--- modules/plugin/python/dls2_py/__init__.py
+++ modules/plugin/python/dls2_py/__init__.py
@@ -1,3 +1,13 @@
-from .periodic_app import wrap_periodic_app, wrap_periodic_app_plugin
+from .app import wrap_app
+from .periodic_app_plugin import wrap_periodic_app_plugin
+from .plugin import InputInfo, OutputInfo, Plugin
+from .periodic_app import wrap_periodic_app
 
-__all__ = ["wrap_periodic_app", "wrap_periodic_app_plugin"]
+__all__ = [
+    "InputInfo",
+    "OutputInfo",
+    "Plugin",
+    "wrap_app",
+    "wrap_periodic_app",
+    "wrap_periodic_app_plugin",
+]
