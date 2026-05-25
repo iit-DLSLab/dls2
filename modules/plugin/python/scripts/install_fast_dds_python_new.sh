f273b965e071796bfd515e7a2a572f5e50fea297
mich-pest <michele.pestarino@outlook.it>
2026-05-25 16:09:57 +0200

    python: fastdds-python docker-oriented installation

--- modules/plugin/python/scripts/install_fast_dds_python_new.sh
+++ modules/plugin/python/scripts/install_fast_dds_python_new.sh
@@ -1,13 +1,24 @@
+#!/bin/bash
 
-# Python-related installation (put in dockerfile)
+set -euo pipefail
+
+WORKSPACE_ROOT="${HOME}/fastdds_python_3p5_ws"
+FASTDDS_PYTHON_TAG="v2.5.0"
+PYTHON_SITE_PACKAGES="${WORKSPACE_ROOT}/install/fastdds_python/lib/python3.12/site-packages"
+
 sudo apt update
-sudo apt install swig4.1 libpython3-dev libtinyxml2-10
+sudo apt install -y swig4.1 libpython3-dev libtinyxml2-10
 pip3 install -U colcon-common-extensions vcstool --break-system-packages
 
-mkdir -p ~/fastdds_python_3p5_ws/src
-cd ~/fastdds_python_3p5_ws/src
-git clone --branch v2.5.0 --depth 1 https://github.com/eProsima/Fast-DDS-python.git fastdds_python
-cd ..
+mkdir -p "${WORKSPACE_ROOT}/src"
+cd "${WORKSPACE_ROOT}/src"
+
+if [ ! -d fastdds_python/.git ]; then
+    git clone --branch "${FASTDDS_PYTHON_TAG}" --depth 1 \
+        https://github.com/eProsima/Fast-DDS-python.git fastdds_python
+fi
+
+cd "${WORKSPACE_ROOT}"
 colcon build \
   --packages-select fastdds_python \
   --cmake-args \
@@ -17,13 +28,20 @@ colcon build \
     -Dfastcdr_DIR=/usr/local/lib/cmake/fastcdr \
     -DCMAKE_INSTALL_RPATH=/usr/local/lib
 
-
-source ~/fastdds_python_3p5_ws/install/setup.bash
-export FASTDDS_PYTHON_INSTALL_ROOT=~/fastdds_python_3p5_ws/install
-export PYTHONPATH=~/fastdds_python_3p5_ws/install/fastdds_python/lib/python3.12/site-packages:$PYTHONPATH
-export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+export COLCON_TRACE="${COLCON_TRACE-}"
+export AMENT_TRACE_SETUP_FILES="${AMENT_TRACE_SETUP_FILES-}"
+set +u
+source "${WORKSPACE_ROOT}/install/setup.bash"
+set -u
+export FASTDDS_PYTHON_INSTALL_ROOT="${WORKSPACE_ROOT}/install"
+export PYTHONPATH="${PYTHON_SITE_PACKAGES}:${PYTHONPATH:-}"
+export LD_LIBRARY_PATH="/usr/local/lib:${LD_LIBRARY_PATH:-}"
 
 python3 - <<'PY'
 import fastdds
 print("fastdds import ok:", fastdds.__file__)
 PY
+
+export FASTDDS_PYTHON_INSTALL_ROOT=${WORKSPACE_ROOT}/install
+export PYTHONPATH=${PYTHON_SITE_PACKAGES}:\$PYTHONPATH
+export LD_LIBRARY_PATH=/usr/local/lib:\$LD_LIBRARY_PATH
