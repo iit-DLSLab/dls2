4359572ccdecc84d1ca3eb4d915e8daadd48336b
mich-pest <michele.pestarino@outlook.it>
2026-06-12 10:51:28 +0200

    python: wrap_app removed

--- modules/plugin/python/dls2_py/__init__.py
+++ modules/plugin/python/dls2_py/__init__.py
@@ -1,4 +1,3 @@
-from .app import wrap_app
 from .periodic_app_plugin import create_periodic_app_plugin, wrap_periodic_app_plugin
 from .plugin import InputInfo, OutputInfo, Plugin
 from .periodic_app import wrap_periodic_app
@@ -8,7 +7,6 @@ __all__ = [
     "OutputInfo",
     "Plugin",
     "create_periodic_app_plugin",
-    "wrap_app",
     "wrap_periodic_app",
     "wrap_periodic_app_plugin",
 ]
